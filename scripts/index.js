1781e0b108a6b20f0a804ec2b93d5b856eaad84c
karpagam <karpagam-s@pluralsight.com>
2024-05-13 14:46:36 +0530

    added select option

--- scripts/index.js
+++ scripts/index.js
@@ -33,6 +33,9 @@ let teams = [
  {
     const footballList = document.getElementById("footballList");
 
+    let theOption1 = new Option("Select a team", ""); 
+    footballList.appendChild(theOption1);
+
     for (let i = 0; i < teams.length; i++) {
 
         // create the option element and set the text and
@@ -49,14 +52,17 @@ let teams = [
   {
     const footballList1 = document.getElementById("footballList");
     let selectedvalue=footballList1.value; 
-   
-    let team=teams.find(c=>c.teamcode==selectedvalue);
     let textContent="";
-
-const containerp = document.getElementById("containerp");
-   textContent = `You selected the ${team.name} (${team.teamcode}) who play in ${team.locatedIn}`;
-       
-  containerp.innerText=textContent;
-
+    if(selectedvalue !="")
+    {
+        let team=teams.find(c=>c.teamcode==selectedvalue);
+        const containerp = document.getElementById("containerp");
+        textContent = `You selected the ${team.name} (${team.teamcode}) who play in ${team.locatedIn}`;
+    }
+    containerp.innerText=textContent;
+   
   }
+  
+
+  
   
